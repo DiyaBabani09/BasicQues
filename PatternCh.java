ca4f1dfd4204eda76c6dfba8e08e717a68257f05
DiyaBabani09 <diyababani03@gmail.com>
2025-08-21 18:22:22 +0530

    add new file

---
+++ PatternCh.java
@@ -0,0 +1,15 @@
+public class PatternCh{
+public static void main(String args[]){
+int n=7;
+System.out.println("pattern1");
+char ch='A';
+for(int i=1;i<=n;i++){
+for(int j=1;j<=i;j++){
+System.out.print(ch+ " ");
+ch++;
+}
+System.out.println();
+
+}
+}
+}
